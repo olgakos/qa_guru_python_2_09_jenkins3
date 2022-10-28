717dcd95fec8a8370555ab157e43208c7b52f3c6
Olga <49266934+olgakos@users.noreply.github.com>
2022-10-28 15:07:56 +0300

    Update registration_form.py

--- demoqa_tests/model/pages/registration_form.py
+++ demoqa_tests/model/pages/registration_form.py
@@ -8,12 +8,25 @@ from demoqa_tests.model.controls import dropdown, date
 from demoqa_tests.utils import path
 from tests.test_data.users import Subject
 
+#---------------1
+@pytest.fixture(autouse=True)
+def browser_management():
+    browser.config.timeout = 3
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.browser_name = 'chrome'
+    browser.config.window_width = 1024
+    browser.config.window_height = 2000
+    yield
+#---------------2  
+    
 def given_opened(browser):
     browser.open('http://demoqa.com/automation-practice-form')
-    #если есть рекламынй блок (3):
+#---------------1
+    #если есть рекламный блок (3):
     ads = ss('[id^=google_ads][id$=container__]')
     if ads.with_(timeout=15).wait.until(have.size_greater_than_or_equal(3)):
         ads.perform(command.js.remove)
+#---------------2
 
 #@allure.step('Add subjects {values}')
 def add_subjects(values: Tuple[Subject], browser):
