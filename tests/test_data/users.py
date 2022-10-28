c77d2f2694c8287262cd2f4172756e3894ff9846
Olga <49266934+olgakos@users.noreply.github.com>
2022-10-28 12:37:41 +0300

    Create users.py

---
+++ tests/test_data/users.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Tuple
+
+
+class Subject(Enum):
+    History = 'History'
+    Maths = 'Maths'
+    Physics = 'Pysics'
+
+
+class Hobby(Enum):
+    Sports = '1'
+    Reading = '2'
+    Music = '3'
+
+
+class Gender(Enum):
+    Male = 'Male'
+    Female = 'Female'
+    Other = 'Other'
+
+
+@dataclass
+class User:
+    gender: Gender
+    name: str
+    last_name: str = 'OlgaKos'
+    email: str = 'test@test.com'
+    user_number: str = '0123456789'
+    birth_day: str = '01'
+    birth_month: str = 'January'
+    birth_year: str = '2000'
+    subjects: Tuple[Subject] = (Subject.History,)
+    current_address: str = 'ST.Petegburg'
+    hobbies: Tuple[Hobby] = (Hobby.Reading,)
+    picture_file: str = 'siegfriedsassoon.jpg'
+    state: str = 'Haryana'
+    city: str = 'Karnal'
+
+
+student = User(name='OlgaKos', gender=Gender.Female)
