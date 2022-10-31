2b47f6eea2dbb7b03462c109374114d88c011a1b
Olga <49266934+olgakos@users.noreply.github.com>
2022-10-31 10:06:28 +0300

    Update registration_form.py

--- demoqa_tests/model/pages/registration_form.py
+++ demoqa_tests/model/pages/registration_form.py
@@ -10,14 +10,14 @@ from tests.test_data.users import Subject
 
 def given_opened(browser):
     browser.open('http://demoqa.com/automation-practice-form')
-
+'''
     #рекламные блоки(!)
     #var1
     ads = ss('[id^=google_ads][id$=container__]')
         if ads.with_(timeout=15).wait.until(have.size_greater_than_or_equal(2)):
             ads.perform(command.js.remove)
-'''
-    #var22     
+
+    #var2     
     ads = browser.all('[id^=google_ads_][id$=container__]')
         if ads.wait.until(have.size_less_than_or_equal(2)):
             ads.perform(command.js.remove)            
