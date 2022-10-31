04cde123ba5347d867d1bd1b4d3b9121bf198143
Olga <49266934+olgakos@users.noreply.github.com>
2022-10-31 10:05:02 +0300

    Update registration_form.py

--- demoqa_tests/model/pages/registration_form.py
+++ demoqa_tests/model/pages/registration_form.py
@@ -10,18 +10,18 @@ from tests.test_data.users import Subject
 
 def given_opened(browser):
     browser.open('http://demoqa.com/automation-practice-form')
-'''
+
     #рекламные блоки(!)
     #var1
     ads = ss('[id^=google_ads][id$=container__]')
-        if ads.with_(timeout=15).wait.until(have.size_greater_than_or_equal(4)):
+        if ads.with_(timeout=15).wait.until(have.size_greater_than_or_equal(2)):
             ads.perform(command.js.remove)
 '''
     #var22     
     ads = browser.all('[id^=google_ads_][id$=container__]')
         if ads.wait.until(have.size_less_than_or_equal(2)):
             ads.perform(command.js.remove)            
-  
+  '''
 
 #@allure.step('Add subjects {values}')
 def add_subjects(values: Tuple[Subject], browser):
